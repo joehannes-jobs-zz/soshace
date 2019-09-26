7152263676b27c6a94a5574f419891c215084ada
DI (FH) Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2019-09-26 15:12:31 -0400

    messy hacky thingie

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component<{}, { title: string, opening_crawl: any, starships: Array<string> }> {
+
+	componentWillMount() {
+		const promise = fetch('https://swapi.co/api/films/1/');
+		promise.then((val) => {
+			let movie = JSON.parse(val);
+			this.setState({ title: movie!.title, opening_crawl: movie!.opening_crawl, starships: movie!.starships });
+		})
+
+		this.state = {
+			title: "",
+			opening_crawl: "",
+			starships: [],
+		};
+	}
+	render() {
+		const { title, opening_crawl, starships } = this.state;
+
+		return (
+			<div className="App">
+				<ul>
+					<li>title: movie.title</li>
+					<li>opening crawl: movie.opening_crawl</li>
+					<li>
+						starships:
+						<ul>
+							{movie.starships.length && (movie!.starships as Array<string>).map(url => {
+								return (
+									<li key={url}>
+										<a href={url}>{url}</a>
+									</li>
+								);
+							})}
+						</ul>
+					</li>
+				</ul>
+			</div>
+		);
+	}
 }
 
 export default App;
